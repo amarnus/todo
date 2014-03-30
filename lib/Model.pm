acf72dce6212b5c9efa062eb8e1065a9b07bc7ae
Amarnath Ravikumar <amarnus@gmail.com>
2014-03-30 20:05:55 +0530

    Added integration with Mozilla Persona for authentication. Added session handling with Dancer sessions.

--- lib/Model.pm
+++ lib/Model.pm
@@ -7,21 +7,24 @@ use MongoDB;
 use JSON;
 use Data::Dumper;
 use Dancer::Logger;
+use LWP::UserAgent;
+use HTTP::Request::Common qw{ POST };
 
 $VERSION  = 1.00;
 @ISA      = qw(Exporter);
-@EXPORT   = qw(getTodos getCompletedTodos getArchivedTodos saveTodo archiveTodo completeTodo);
+@EXPORT   = qw(getTodos getCompletedTodos getArchivedTodos saveTodo archiveTodo updateTodo lookupOrRegisterUser);
 
 sub initCollection {
   Dancer::Logger::debug ("Setting up the Database connection..\n");
   my $client = MongoDB::MongoClient->new;
-  my $database = $client->get_database( 'todo' );
+  our $database = $client->get_database( 'todo' );
   our $collection = $database->get_collection( 'todos' );
 };
 
 sub getTodos {
+  my ($account) = @_;
   defined(our $collection) or initCollection();
-  my @todos = $collection->find()->all;
+  my @todos = $collection->find({ 'email' => $account->{'email'} })->all;
   return @todos;
 };
 
@@ -49,8 +52,32 @@ sub archiveTodo {
   $collection->update({ _id => MongoDB::OID->new($oid)  }, { '$set' => { 'archived' => 1 } });
 };
 
-sub completeTodo {
-  my ($todo) = @_;
+sub updateTodo {
+  my ($todo_id, $todo) = @_;
   defined(our $collection) or initCollection();
-  $collection->update($todo, { 'status' => 1 });
+  delete $todo->{'_id'};
+  $collection->update({ _id => MongoDB::OID->new($todo_id) }, $todo);
+};
+
+sub lookupOrRegisterUser {
+  my ($persona_key) = @_;
+  defined(our $collection) or initCollection();
+  our $database;
+  my $collection = $database->get_collection('users');
+  my $ua = LWP::UserAgent->new();
+  my $request = POST ( 'https://verifier.login.persona.org/verify',
+  [ 'assertion' => $persona_key, 'audience' => 'http://localhost:3000' ]);
+  my $json = JSON->new->utf8->allow_blessed->convert_blessed;
+  $ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
+  my $ua = LWP::UserAgent->new();
+  my $response = $ua->request($request);
+  my $user_info = $json->decode($response->content);
+  if ($user_info->{'status'} eq 'okay') {
+    my $account = $collection->find_one({ email => $user_info->{'email'} });
+    if (!defined($account)) {
+      my $account = { email => $user_info->{'email'}, issuer => $user_info->{'issuer'} };
+      $collection->save($account);
+    }
+    return $account;
+  }
 };
--- lib/todo.pm
+++ lib/todo.pm
@@ -2,6 +2,7 @@ package todo;
 use Dancer ':syntax';
 use Model;
 use Data::Dumper;
+use Template;
 
 our $VERSION = '0.1';
 
@@ -9,14 +10,43 @@ sub initJson {
   our $json = JSON->new->utf8->allow_nonref->allow_blessed->convert_blessed;
 };
 
+sub getUser {
+  return session('user');
+};
+
 sub getTodosRouteHandler {
-  my @todos = getTodos();
+  my $account = getUser();
+  my @todos = getTodos($account);
   my $json = JSON->new->utf8->allow_nonref->allow_blessed->convert_blessed;
   return $json->encode(\@todos);
 };
 
 get '/' => sub {
-    template 'index';
+  my $logged_in = false;
+  my $logged_out = true;
+  if (session('user')) {
+    $logged_in = true; $logged_out = false;
+  }
+  template 'index' =>
+    { 'logged_in' => $logged_in, 'logged_out' => $logged_out  };
+};
+
+post '/api/persona/login' => sub {
+  header 'Content-Type' => 'application/json';
+  defined(our $json) or initJson();
+  my $req = $json->decode(request->body);
+  my $persona_key = $req->{'persona_key'};
+  my $account = lookupOrRegisterUser($persona_key);
+  if (!defined($account)) {
+    status 401;
+  }
+  session 'user' => $account;
+  status 200;
+};
+
+get '/user/logout' => sub {
+  session->destroy;
+  redirect '/';
 };
 
 get '/api/todos' => sub {
@@ -43,7 +73,12 @@ post '/api/todos' => sub {
   header 'Content-Type' => 'application/json';
   defined(our $json) or initJson();
   my $req = $json->decode(request->body);
-  my $todo_id = saveTodo($req->{'todo'});
+  my $todo = $req->{'todo'};
+  my $account = getUser();
+  if (defined($account)) {
+    $todo->{'email'} = $account->{'email'};
+  }
+  my $todo_id = saveTodo($todo);
   return $json->encode({'todo_id' => $todo_id});
 };
 
@@ -54,11 +89,12 @@ post '/api/todos/archive' => sub {
   archiveTodo($req->{'todo'}->{'oid'});
 };
 
-post '/api/todos/complete' => sub {
+put '/api/todo/:todo_id' => sub {
   header 'Content-Type' => 'application/json';
   defined(our $json) or initJson();
+  my $todo_id = params->{'todo_id'};
   my $req = $json->decode(request->body);
-  completeTodo($req->{'todo'});
+  updateTodo($todo_id, $req->{'todo'});
 };
 
 true;
